4edccc2d79f9cbe7cb2d3df0e35741b3e6766e5f
haris sucipto <harisssucipto@gmail.com>
2020-04-09 17:38:03 +0700

    add signin stateless component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 
+import Signin from "./Signin";
+
 const Auth = () => {
   return (
     <div>
-      <Button
-        style={{
-          marginRight: "20px",
-        }}
-      >
-        Sign in
-      </Button>
+      <Signin />
       <Button variant="contained">Sign up</Button>
     </div>
   );
