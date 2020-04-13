2e7340a4d89eec264fbe088f58eabf7bbef1c6b2
haris sucipto <harisssucipto@gmail.com>
2020-04-13 08:54:04 +0700

    build1

---
+++ docs/precache-manifest.d9b8c93e91326003cba77524a91065d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890cf2f4dabd36146ef55db15a46beba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a10dc6a03d127be1d8",
+    "url": "/static/css/main.664cbcc4.chunk.css"
+  },
+  {
+    "revision": "aef8352ad937ad34409d",
+    "url": "/static/js/2.9804e018.chunk.js"
+  },
+  {
+    "revision": "89d3385f7d39af76bd84ec1c3fe43df2",
+    "url": "/static/js/2.9804e018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a10dc6a03d127be1d8",
+    "url": "/static/js/main.af75cbe6.chunk.js"
+  },
+  {
+    "revision": "93376725e8e9b1cd20f6",
+    "url": "/static/js/runtime-main.ddc3bcb1.js"
+  },
+  {
+    "revision": "14c898c5962284dd1e0ea9d43e2cb40c",
+    "url": "/static/media/haris.14c898c5.jpg"
+  },
+  {
+    "revision": "84a99bec376612a8bdfe170a8603656a",
+    "url": "/static/media/logo.84a99bec.PNG"
+  },
+  {
+    "revision": "79bd1e86e9707bdc2a6a70a93b1a7d0d",
+    "url": "/static/media/p1.79bd1e86.png"
+  },
+  {
+    "revision": "446b1b123961a22019b387f66291f955",
+    "url": "/static/media/p1extra1.446b1b12.png"
+  },
+  {
+    "revision": "c32604e792e89a014de639bb6976430a",
+    "url": "/static/media/p1extra2.c32604e7.png"
+  },
+  {
+    "revision": "d79f62a56240a2f077f5ef0d477c30a9",
+    "url": "/static/media/p1extra3.d79f62a5.png"
+  },
+  {
+    "revision": "e0aab776f5d501d59b0361e961c9cfd0",
+    "url": "/static/media/p2.e0aab776.png"
+  },
+  {
+    "revision": "a37bf6059391dad771ee3134047af960",
+    "url": "/static/media/p3.a37bf605.png"
+  },
+  {
+    "revision": "d94799514d2f931d20b143a2b53f4a17",
+    "url": "/static/media/p4.d9479951.png"
+  },
+  {
+    "revision": "22eff0fd24f33d85553d700bf7acb359",
+    "url": "/static/media/sample-property.22eff0fd.PNG"
+  }
+]);
